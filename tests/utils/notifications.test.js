5c587567a89d0beee2f61fdf580ac7ddcdcc01b6
jaskaran <jaskaran.sarkaria@googlemail.com>
2020-06-27 16:49:39 +0100

    test: 💍 remove irrelevant tests

--- tests/utils/notifications.test.js
+++ tests/utils/notifications.test.js
@@ -11,26 +11,4 @@ describe('how notifications are initialised and pushed', () => {
       await checkPermissions();
       expect(notificationSpy).toBeCalled();
     });
-
-  it('when asked permission is denied display "what\'s the point?" page', async () => {
-    global.Notification = {
-      requestPermission: jest.fn(),
-      permission: 'denied',
-    };
-    const result = await checkPermissions();
-    expect(result).toBe('denied');
-  });
-
-  it('when permision is granted show the timer', async () => {
-    global.Notification = {
-      requestPermission: jest.fn(),
-      permission: 'granted',
-    };
-    const result = await checkPermissions();
-    expect(result).toBe('granted');
-  });
-
-  it.skip('When the timer finishes notify the new driver to restart the timer', () => {});
-  it.skip('when the timer finishes and user is not the driver, notify' +
-  'them that the next session will restart when the driver is ready', () => {});
 });
